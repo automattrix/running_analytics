c04fda36e40250ba7db753f631701ecd502ac841
automattrix <matt.karan93@gmail.com>
2021-08-03 16:46:28 -0700

    add main function and remove old auth location

---
+++ running_analytics.py
@@ -0,0 +1,19 @@
+from lib.AthleteData import Athlete
+from lib.utils.environment.env import set_env
+import os
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(10)
+
+
+def main():
+    athlete_insights = Athlete(name='Matt Karan')
+    athlete_insights.authorize()
+
+
+if __name__ == '__main__':
+    logger.debug(os.path.dirname(os.path.realpath(__file__)))
+    set_env(project_path=os.path.dirname(os.path.realpath(__file__)))
+    main()
