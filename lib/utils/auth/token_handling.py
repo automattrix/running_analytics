c2b0bb37f28670fb098e8aeebeb4e9afae155042
automattrix <matt.karan93@gmail.com>
2021-08-05 17:22:39 -0700

    check request status code

--- lib/utils/auth/token_handling.py
+++ lib/utils/auth/token_handling.py
@@ -1,8 +1,13 @@
 import requests
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def request_token(token_params):
+    token_data = None
     url = 'https://www.strava.com/api/v3/oauth/token'
     body = {
         'client_id': token_params['CLIENT_ID'],
@@ -13,4 +18,11 @@ def request_token(token_params):
     headers = {'content-type': 'application/json'}
 
     r = requests.post(url, data=json.dumps(body), headers=headers)
-    print(r.json())
+
+    request_status = r.status_code
+    if request_status == 200:
+        token_data = r.json()
+    else:
+        logger.error(request_status)
+
+    return token_data
